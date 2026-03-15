a08c180a767fb69b8c9fb90f1be0f93eb4f5de02
imamsyahjr-sketch <imamsyahjr@gmail.com>
2026-03-16 05:00:07 +0700

    Update db.php

--- db.php
+++ db.php
@@ -2,12 +2,12 @@
 
 header("Content-Type: application/json");
 
-// ambil environment variable dari Railway
-$host = $_ENV["MYSQLHOST"] ?? "";
-$user = $_ENV["MYSQLUSER"] ?? "";
-$pass = $_ENV["MYSQLPASSWORD"] ?? "";
-$db   = $_ENV["MYSQLDATABASE"] ?? "";
-$port = $_ENV["MYSQLPORT"] ?? "3306";
+// konfigurasi database Railway
+$host = "mysql.railway.internal";
+$user = "root";
+$pass = "LyegTKdPZCZoPDJUDJApSbHGHatrZJmh";
+$db   = "railway";
+$port = 3306;
 
 // koneksi database
 $conn = new mysqli($host, $user, $pass, $db, $port);
