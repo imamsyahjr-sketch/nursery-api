af13ec5ef401ace11495b11acaa30a07acecd570
imamsyahjr-sketch <imamsyahjr@gmail.com>
2026-03-16 01:42:02 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,23 +1,18 @@
 <?php
 
-$host = "sql213.infinityfree.com";
-$user = "if0_41396589";
-$pass = "JI2nhv3BKRK2";
-$db   = "if0_41396589_nursery";
+$host = $_ENV["mysql.railway.internal"];
+$user = $_ENV["root"];
+$pass = $_ENV["hXTrZTSfAHojjCrZWoxJeTMmVuUSSNkH"];
+$db   = $_ENV["railway"];
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host,$user,$pass,$db);
 
-if ($conn->connect_error) {
-
+if($conn->connect_error){
     echo json_encode([
-        "status" => "error",
-        "message" => "Koneksi database gagal"
+        "status"=>"error",
+        "message"=>"Koneksi database gagal"
     ]);
-
     exit;
-
 }
-
-$conn->set_charset("utf8");
 
 ?>
